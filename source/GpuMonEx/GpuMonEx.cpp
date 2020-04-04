381486f8f22d780266a56bafcffa88b969c3e4dc
blueshogun96 <blueshogun96@aim.com>
2020-04-04 04:08:12 -0700

    Using wxWidgets DLL files to save space on Windows

--- source/GpuMonEx/GpuMonEx.cpp
+++ source/GpuMonEx/GpuMonEx.cpp
@@ -5,10 +5,29 @@
 #include "GpuMonEx.h"
 
 #include <wx/wx.h>
+#include <wx/string.h>
+#include <wx/utils.h>
 
 
 class GpuMonEx_MainFrame : public wxFrame
 {
 public:
-	GpuMonEx_MainFrame( const wxString& title );
+	GpuMonEx_MainFrame( const wxString& title ) : wxFrame( NULL, wxID_ANY, title, wxDefaultPosition, wxSize( 960, 540 ) )
+	{
+		Centre();
+	}
 };
+
+class GpuMonEx : public wxApp
+{
+public:
+	virtual bool OnInit()
+	{
+		auto mainframe = new GpuMonEx_MainFrame( wxT( "GpuMonEx (Pre-Alpha)" ) );
+		mainframe->Show();
+
+		return true;
+	}
+};
+
+IMPLEMENT_APP( GpuMonEx );
